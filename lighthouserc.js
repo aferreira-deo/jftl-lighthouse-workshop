9d344e56efccf65a23d35d2837f5f6b3f610eaa5
aferreira-deo <a.ferreira@deo.dev>
2024-06-04 16:45:52 +0200

    add lighthouse on cicd workflow

---
+++ lighthouserc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  ci: {
+    collect: {
+      // url: ['http://192.168.1.2:8080'],
+      url: ['https://aferreira-deo.github.io/jftl-lighthouse-workshop/'],
+    },
+    assert: {
+      assertions: {
+        'categories:accessibility': ['error', {'minScore': 0.9}]
+      }
+    },
+    upload: {
+      target: 'temporary-public-storage',
+    },
+  },
+};
+  
